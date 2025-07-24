4cdbac8fb256dcc622fbb56a3416995354ce655b
Paulo Souza <paulo_aero@hotmail.com>
2025-07-24 16:35:04 -0300

    Update are_we_there_yet.py

--- challenge_pack_loops/are_we_there_yet.py
+++ challenge_pack_loops/are_we_there_yet.py
@@ -1,5 +1,7 @@
 #Are We There Yet?
-answer = input("Are we there yet? ")
-while answer != "Are we there yet? Yes":
-    answer = input("Are we there yet? ")
-    print(answer)
+
+answer = input("Are we there yet? ").upper() #Not exercise requirement
+while answer != "YES":
+    answer = input("Are we there yet? ").upper() #.upper() added to accept upper and lower case letters
+    if answer == "YES":
+        print("WE MADE IT!")
